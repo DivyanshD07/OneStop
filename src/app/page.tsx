690b8efb2a7eb85be02d78b295425de08b4d4767
Divyansh Dhyani <94350084+DivyanshD07@users.noreply.github.com>
2024-05-05 04:14:13 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ const HomePage = () => {
 
   return (
     <h1 className="flex justify-center items-center p-5 text-green-500 text-lg font-bold">
-      <DashBoard />
+      <Login />
     </h1>
   );
 };
