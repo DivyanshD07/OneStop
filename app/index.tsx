0df658ad1b0c3617cb6532ec41bd2d6cdf09ab92
Divyansh Dhyani <94350084+DivyanshD07@users.noreply.github.com>
2024-05-06 02:11:50 +0530

    Delete app directory

--- app/index.tsx
+++
@@ -1,6 +0,0 @@
-import Hero from "./DashBoard/page";
-import Navbar from "../components/Navbar";
-import './globals.css';
-
-export {default as Home} from './page';
-export {default as RootLayout} from './layout';
