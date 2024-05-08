45bb98adbec02bcd6ad3c897846f57fd2bfdea8b
Divyansh Dhyani <94350084+DivyanshD07@users.noreply.github.com>
2024-05-08 10:19:15 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <footer className="w-full flex flex-row justify-around footer text-end text-white mt-12">
-      <div className="text-4xl font-bold ">OneStop</div>
+      <div className="text-4xl font-bold ">LastStop</div>
       <p className="flex justify-around text-center">&copy; OneStop {new Date().getFullYear()}</p>
     </footer>
   )
