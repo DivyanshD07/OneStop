3fe8cb06507a66765b50348d9857cd8d672c3711
Divyansh Dhyani <94350084+DivyanshD07@users.noreply.github.com>
2024-05-08 10:18:53 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 const Footer = () => {
   return (
-    <footer className=" flex flex-row justify-between footer text-end text-white py-6 mt-12">
-      <div className="text-2xl ml-4">OneStop</div>
+    <footer className="w-full flex flex-row justify-around footer text-end text-white mt-12">
+      <div className="text-4xl font-bold ">OneStop</div>
       <p className="flex justify-around text-center">&copy; OneStop {new Date().getFullYear()}</p>
     </footer>
   )
