7c8efa1adab20fb27a697b49e6f954fc2fc9837f
Divyansh Dhyani <94350084+DivyanshD07@users.noreply.github.com>
2024-05-23 19:32:48 +0530

    Update NotesCard.tsx

--- src/components/Cards/NotesCard.tsx
+++ src/components/Cards/NotesCard.tsx
@@ -1,24 +1,24 @@
-// import './NotesCard.css';
 import React from 'react';
 import Link from 'next/link';
 import { FaCircle, FaDownload } from 'react-icons/fa6';
-import { FaDotCircle } from 'react-icons/fa';
-
 
 interface NotesInfo {
     id: number;
     name: string;
+    download: string;
 }
 
 const Card: React.FC<{ Notes: NotesInfo }> = (props) => {
+    const handleDownload = () => {
+        window.open(props.Notes.download, '_blank');
+    };
+
     return (
-        <Link href={`/notes?courseId=${props.Notes.id}`}>
-            <div className="text-white flex justify-center items-center align-middle gap-6">
-                <FaCircle className='h-1/2'/>
-                <h1 className='text-xl'>{props.Notes.name}</h1>
-                <button><FaDownload /></button>
-            </div>
-        </Link>
+        <div className="text-white flex justify-center items-center align-middle gap-6">
+            <FaCircle className='h-1/2' />
+            <h1 className='text-xl'>{props.Notes.name}</h1>
+            <button onClick={handleDownload}><FaDownload /></button>
+        </div>
     );
 };
 
